d08bf22e1c3bda9fbaac8db3eb9f131b6e5fa51f
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-05-17 23:07:04 +0600

    starting template

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/NavBar";
+import Footer from "../pages/Shared/Footer";
+
+const MainLayout = () => {
+    return (
+        <>
+            <NavBar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    );
+};
+
+export default MainLayout;
